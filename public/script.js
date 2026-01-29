a62c9f09fdc3f9d2207d7d8de4fdd19096eb902f
kavinkrishnanavan <nk.kavinkrishna@gmail.com>
2026-01-29 21:23:07 +0800

    l

--- public/script.js
+++ public/script.js
@@ -9,6 +9,7 @@ const mt = document.getElementById("mt");
 const jt = document.getElementById("jt");
 const input = document.getElementById("prompt");
 const gifImg = document.getElementById("gif");
+const speak = document.getElementById("speak")
 const Gifs = {
   prerequisite: "https://media1.tenor.com/m/-koXelHpdokAAAAd/before-we-begin-emma.gif",
   modesty: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcTdxbGYzemxkMzg5MGs0b2NleXFvd3BoemExZTRhcnRrNmYwdWM2NSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/qN9KLSKynX1bNqNzFY/giphy.gif",
@@ -19,11 +20,37 @@ const Gifs = {
 
 const audio = document.getElementById("audio");
 
-
-
 function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
+
+speak.addEventListener('click', async function() {
+    const text = "[cheerful] Word: " + answer[0] + ". Meaning: " + answer[1] + ". Joke: " + answer[2];
+    
+    if (!text) return;
+
+    try {
+        const res = await fetch("/.netlify/functions/tts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ text })
+        });
+
+        const blob = await res.blob();
+        const url = URL.createObjectURL(blob);
+
+        audio.src = url;
+        audio.play();
+    } catch (err) {
+        console.error(err);
+        console.log("Text-to-speech failed");
+    } finally {
+        // Do Nothing
+    }
+});
+
 
 button.addEventListener("click", async () => {
   output.textContent = "Making...";
@@ -61,30 +88,7 @@ button.addEventListener("click", async () => {
 
     const query = input.value.trim().toLowerCase();
 
-    const text = "[cheerful]" + "Word: " + answer[0] + ". Meaning: " + answer[1] + ". Joke: " + answer[2];
-          
-    if (!text) return;
-
-    try {
-      const res = await fetch("/.netlify/functions/tts", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ text })
-        });
-
-      const blob = await res.blob();
-      const url = URL.createObjectURL(blob);
-
-      audio.src = url;
-      audio.play();
-    } catch (err) {
-      console.error(err);
-      console.log("Text-to-speech failed");
-    } finally {
-      // Do Nothing
-    }
+    
 
     if (!query) return;
 
