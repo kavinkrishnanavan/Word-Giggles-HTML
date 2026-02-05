36fe47f688b0396eda95df65152d42f61364ab64
kavinkrishnanavan <nk.kavinkrishna@gmail.com>
2026-02-05 15:09:38 +1100

    Speak meaning

--- public/script.js
+++ public/script.js
@@ -11,6 +11,7 @@ const input = document.getElementById("prompt");
 const gifImg = document.getElementById("gif");
 const speak = document.getElementById("speakf")
 const speakword = document.getElementById("speakword")
+const speakmeaning = document.getElementById("speakmeaning")
 const Gifs = {
   prerequisite: "https://media1.tenor.com/m/-koXelHpdokAAAAd/before-we-begin-emma.gif",
   modesty: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcTdxbGYzemxkMzg5MGs0b2NleXFvd3BoemExZTRhcnRrNmYwdWM2NSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/qN9KLSKynX1bNqNzFY/giphy.gif",
@@ -27,11 +28,39 @@ function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
+speakmeaning.addEventListener('click', async function() {
+
+    if (!meaning.textContent) return;
+
+    const text = "[cheerful]" + meaning.textContent;
+
+    try {
+        const res = await fetch("/.netlify/functions/tts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ text })
+        });
+
+        const blob = await res.blob();
+        const url = URL.createObjectURL(blob);
+
+        audio.src = url;
+        audio.play();
+    } catch (err) {
+        console.error(err);
+        console.log("Text-to-speech failed");
+    } finally {
+        // Do Nothing
+    }
+});
+
 speakword.addEventListener('click', async function() {
 
     if (!word.textContent) return;
 
-    const text = "[cheerful] Word. " + word.textContent;
+    const text = "[cheerful]" + word.textContent;
 
     try {
         const res = await fetch("/.netlify/functions/tts", {
